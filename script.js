d8572847fc0806a625597e3c55264ff145495d6f
vlad-rus-work <vlad.rus.work@gmail.com>
2025-06-19 11:02:01 +0300

    Finished the project, final commit.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Global variables
+let humanScore = 0;
+let computerScore = 0;
+let rounds = 1;
+
 // Generate the "random" computer output
 // We have 1 of 3 choices, so we will use a randomly generated number from 1 to 3
 function randomIntFromInterval(min, max) {
@@ -5,11 +10,8 @@ function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const rndInt = randomIntFromInterval(1, 3);
-console.log(rndInt);
+let rndInt = randomIntFromInterval(1, 3);
 // With this number we have to make the 3 cases for the computer
-
-const computerHand = randomHand(rndInt);
 
 function randomHand(rndInt) {
   switch (rndInt) {
@@ -21,13 +23,54 @@ function randomHand(rndInt) {
       return "scissors";
   }
 }
-console.log(computerHand);
 
 // Then we will need to compare it against the user input
-// First we get the user input
-let person = prompt("Rock, paper, scissors!");
-console.log(person);
+// First we get the user input, and make it case-insensitive
+// let userInput = prompt("Rock, paper, scissors!");
+function getHumanChoice() {
+  return (userInput = prompt("Rock, paper, scissors!")).toLowerCase();
+}
+// console.log(getHumanChoice());
+
 // Then we create the compare logic
+function playRound() {
+  rndInt = randomIntFromInterval(1, 3);
+  let computer = randomHand(rndInt);
+  console.log(computer);
+  let player = getHumanChoice();
+  if (computer === player) return "Draw!";
+  if (computer === "rock" && player === "scissors") {
+    computerScore++;
+    return "You lose! Rock beats scissors.";
+  }
+  if (computer === "scissors" && player === "paper") {
+    computerScore++;
+    return "You lose! Scissors beats paper.";
+  }
+  if (computer === "paper" && player === "rock") {
+    computerScore++;
+    return "You lose! Paper beats rock.";
+  }
+  if (player === "rock" && computer === "scissors") {
+    humanScore++;
+    return "You win! Rock beats scissors.";
+  }
+  if (player === "scissors" && computer === "paper") {
+    humanScore++;
+
+    return "You win! Scissors beats paper.";
+  }
+  if (player === "paper" && computer === "rock") {
+    humanScore++;
+    return "You win! Paper beats rock.";
+  }
+}
+
+while (rounds <= 5) {
+  rounds++;
+  playRound();
+  console.log(humanScore, computerScore);
+}
+
 // Rock beats scissors, scissors beats paper, paper beats rock
-function rockPaperScissors(computerHand, person) {}
 // Decide which one is the winner, which one is the loser, and have a case for ties aswell
